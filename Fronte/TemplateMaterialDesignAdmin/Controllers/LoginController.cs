9aa0053d55c157000b067f55aaf400a4148b25e4
Lucas Santos <lucas.santos@otacom.com.br>
2020-12-10 23:16:02 -0300

    Inserindo Login com Auth0

--- Fronte/TemplateMaterialDesignAdmin/Controllers/LoginController.cs
+++ Fronte/TemplateMaterialDesignAdmin/Controllers/LoginController.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Auth0.AuthenticationApi;
+using Auth0.AuthenticationApi.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using TemplateMaterialDesignAdmin.Models.Login.Login;
 
 namespace TemplateMaterialDesignAdmin.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IConfiguration _configuration;
+        private readonly AuthenticationApiClient _client;
+
+        public LoginController(IConfiguration configuration, AuthenticationApiClient client)
+        {
+            _configuration = configuration;
+            _client = client;
+        }
+
         // GET: LoginController
         public ActionResult Index()
         {
@@ -14,21 +33,70 @@ namespace TemplateMaterialDesignAdmin.Controllers
         // POST: LoginController/Login
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Login(IFormCollection collection)
+        public async Task<IActionResult> Login(LoginViewModel vm)
         {
-            try
+            if (ModelState.IsValid)
             {
-                return RedirectToAction("Index", "Home");
+                try
+                {
+                    var result = await _client.GetTokenAsync(new ResourceOwnerTokenRequest
+                    {
+                        ClientId = _configuration["Auth0:ClientId"],
+                        ClientSecret = _configuration["Auth0:ClientSecret"],
+                        Scope = "openid profile",
+                        Realm = "Username-Password-Authentication", // Specify the correct name of your DB connection
+                        Username = vm.EmailAddress,
+                        Password = vm.Password
+                    });
+
+                    // Get user info from token
+                    var user = await _client.GetUserInfoAsync(result.AccessToken);
+
+                    // Create claims principal
+                    var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new[]
+                    {
+                        new Claim(ClaimTypes.NameIdentifier, user.UserId),
+                        new Claim(ClaimTypes.Name, user.FullName)
+
+                    }, CookieAuthenticationDefaults.AuthenticationScheme));
+
+                    // Sign user into cookie middleware
+                    await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (Exception)
+                {
+                    return RedirectToAction("Index", "Login", vm);
+                }
             }
-            catch
-            {
-                return View();
-            }
+
+            return RedirectToAction("Index", "Login", vm);
+        }
+
+        [HttpGet]
+        public async Task LoginExternal(string connection, string returnUrl = "/")
+        {
+            var properties = new AuthenticationProperties() { RedirectUri = returnUrl };
+
+            if (!string.IsNullOrEmpty(connection))
+                properties.Items.Add("connection", connection);
+
+            await HttpContext.ChallengeAsync("Auth0", properties);
         }
 
         // GET: LoginController/Logout
         public ActionResult Logout()
         {
+            //await HttpContext.SignOutAsync("Auth0", new AuthenticationProperties
+            //{
+            //    // Indicate here where Auth0 should redirect the user after a logout.
+            //    // Note that the resulting absolute Uri must be whitelisted in the
+            //    // **Allowed Logout URLs** settings for the client.
+            //    RedirectUri = Url.Action("Index", "Login")
+            //});
+            //await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
             return RedirectToAction("Index", "Login");
         }
 
