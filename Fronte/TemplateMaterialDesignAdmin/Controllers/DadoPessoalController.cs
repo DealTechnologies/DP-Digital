0205e31ba5031533b3b7984a22fde6a3fbff2b65
Silas Júnior <75258708+silas-deal@users.noreply.github.com>
2020-12-12 18:08:26 -0300

    Alteração layout da tela
    Alteração dos layouts da tela;
    inclusão de páginas e imagens;

---
+++ Fronte/TemplateMaterialDesignAdmin/Controllers/DadoPessoalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateMaterialDesignAdmin.Controllers
+{
+    public class DadoPessoalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Fronte/TemplateMaterialDesignAdmin/Controllers/GestaoAtividadeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateMaterialDesignAdmin.Controllers
+{
+    public class GestaoAtividadeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
